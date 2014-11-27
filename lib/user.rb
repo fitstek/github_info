b6cf62b1e112ac81fce3dc442105b081892e0d96
Fitsum <fitsum.teklehaimanot@gmail.com>
2014-11-27 00:59:31 +0000

    created the uri from username

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,12 @@
+require 'uri'
 class User
   attr_reader :username
+
   def initialize username
     @username = username
+  end
+
+  def uri
+    URI("https://api.github.com/users/#{username}/repos")
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,5 +6,8 @@ describe User do
     it 'knows the username' do
       expect(user.username).to eq 'fitstek'
     end
+    it 'can form a uri object when given a username' do
+      expect(user.uri.class).to eq URI::HTTPS
+    end
   end
 end
